31ab4c3871aa34a36b3757059462193ed02a8f31
Karel Boer <karel1982@gmail.com>
2021-03-17 19:58:10 -0500

    card progres

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import delacasa from '../images/delacasa.jpeg'
 
+
 function Card(){
   return(
-    <div className="flex items-stretch grid grid-cols-3 gap-10 bg-gray-800 text-white rounded p-5 m-5">
+    <div className="z-1 overflow-visible flex items-stretch grid grid-cols-3 gap-10 bg-gray-800 text-white rounded p-5 m-5">
         <div className="1">
           <div className="border-b-2 border-white pb-3">
           <h4 className="text-center text-yellow-500">Playa Burger</h4>
@@ -13,7 +14,7 @@ function Card(){
           </div>
         
         </div>
-        <div className="c2 self-center">
+        <div className="c2 self-center font-SyneMono ">
           <p>Carne con tocino, jitomate</p><p>y queso.</p>
         </div>
         <div className="col3 self-center">$3333</div>
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,12 +2,20 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Card from '../components/card'
+// import burger from '../images/hamburger-cheeseburger-veggie-burger-fast-food-burguer-d02a8de84e0ede0b8987bbe0f866487d.png'
+import "./menu.css"
 function MenuPage() {
+  
   return(
     <Layout>
       <SEO />
       <div>
-      <h3 className="text-white">Menu Page</h3>
+        <div className="grid grid-cols-4 text-white">
+          <div className="1">Hamburguesas</div>
+          <div className="2">Extras</div>
+          <div className="3">Refrescos</div>
+          <div className="4">4</div>
+        </div>
       <Card />
       <Card />
       <Card />
