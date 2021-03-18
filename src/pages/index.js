07fdd05032ef0b6b15cd0e8cd51adc972ff924b0
Karel Boer <karel1982@gmail.com>
2021-03-18 08:38:51 -0500

    change foto

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import floatingIMG from "../images/hamburger-cheeseburger-veggie-burger-fast-food-burguer-d02a8de84e0ede0b8987bbe0f866487d.png"
+import floatingIMG from "../images/food-truck-commercial-vehicle-car-barbecue-van-foodtruck-d8567dbea05b2233cae696c9fc884b21.png"
 
 
 function IndexPage() {
@@ -12,19 +12,19 @@ function IndexPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="Home"
       />
-      <div className="grid grid-cols-3 gap-4">
-    <section className=" col-span-1">
-      <h2 className="text-3xl text-yellow-500">Senor burrrrrrr</h2>
-      <p className="text-white font-SyneMono">Nuestro menú es una combinación de sabores ya que usamos los mejores ingredientes para deleitarte.</p> <p>Nuestra carne es escogida y preparada diariamente</p>
-      <button className="bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded">
-        Our Menu
-  Button
-</button>
-    </section>
-    <section className=" col-span-2">
-      <img src={floatingIMG} alt=""/>
-    </section>
-    </div>
+      <div className="grid gap-4">
+        <section className=" col-span-1">
+          <img src={floatingIMG} alt=""/>
+        </section>
+       <section className=" col-span-2">
+        <h2 className="text-3xl text-yellow-500">Senor burrrrrrr</h2>
+        <p className="text-white font-SyneMono">Nuestro menú es una combinación de sabores ya que usamos los mejores ingredientes para deleitarte.</p> <p>Nuestra carne es escogida y preparada diariamente</p>
+        <button className="bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded">
+          Our Menu
+          Button
+        </button>
+       </section>
+      </div>
     </Layout>
   );
 }
