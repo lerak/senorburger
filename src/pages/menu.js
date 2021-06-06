78e449818543b589a585d8a9831a48506e2edc27
Karel Boer <karel1982@gmail.com>
2021-06-06 13:00:07 -0500

    gramatical correction

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -17,14 +17,14 @@ function MenuPage() {
           <h1 className="font-SyneMono text-5xl md:text-6xl text-center text-yellow-500">Burgers</h1>
         </div>
     
-          <Card image={burger} title="Playa Burger" description="Carne con tocino, lechuga, jitomate y queso, acompañada de papas " price="70"/>
-          <Card image={burger} title="Coral Burger" description="Carne con tocino, lechuga, jitomate, queso y cebolla asada, acompañada de papas" price="75"/>
-          <Card image={burger} title="Manglar Burger" description="Carne con tocino, lechuga, jitomate, queso y champiñones, acompañada de papas" price="80"/>
-          <Card image={burger} title="Hawaiiana Burger" description="Carne con tocino, lechuga, jitomate,  queso, jamonon y piña, acompañada de papas" price="85"/>
-          <Card image={burger} title="Ocean Burger" description="Carne con tocino, lechuga, jitomate, cebolla asada y queso azul, acompañada de papas" price="100"/>
-          <Card image={burger} title="De La Casa" description="Carne con tocino, lechuga, jitomate, queso, champiñones y piña, acompañada de papas" price="95"/>
-          <Card image={burger} title="Nohoch" description="Doble carne con tocino, lechuga, jitomate, queso y cebolla asada, acompañada de papas" price="110" />
-          <Card image={burger} title="Selva" description="Carne con tocino, lechuga, jitomate, queso y guacamole" price="95" />
+          <Card image={burger} title="Playa Burger" description="Carne, tocino, lechuga, jitomate y queso, acompañada de papas " price="70"/>
+          <Card image={burger} title="Coral Burger" description="Carne, tocino, lechuga, jitomate, queso y cebolla asada, acompañada de papas" price="75"/>
+          <Card image={burger} title="Manglar Burger" description="Carne, tocino, lechuga, jitomate, queso y champiñones, acompañada de papas" price="80"/>
+          <Card image={burger} title="Hawaiiana Burger" description="Carne, tocino, lechuga, jitomate,  queso, jamon y piña, acompañada de papas" price="85"/>
+          <Card image={burger} title="Ocean Burger" description="Carne, tocino, lechuga, jitomate, cebolla asada y queso azul, acompañada de papas" price="100"/>
+          <Card image={burger} title="De La Casa" description="Carne, tocino, lechuga, jitomate, queso, champiñones y piña, acompañada de papas" price="95"/>
+          <Card image={burger} title="Nohoch" description="Doble carne ,tocino, lechuga, jitomate, queso y cebolla asada, acompañada de papas" price="110" />
+          <Card image={burger} title="Selva" description="Carne, tocino, lechuga, jitomate, queso y guacamole" price="95" />
           <div>
           <h1 className="font-SyneMono text-5xl md:text-6xl text-center text-yellow-500">Extras</h1>
         </div>
