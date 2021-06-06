4bf4eda54a6b6f782567e07ec36980f7c4a2a517
Karel Boer <karel1982@gmail.com>
2021-06-05 23:44:04 -0500

    change menu

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -16,12 +16,12 @@ function MenuPage() {
         <div>
           <h1 className="font-SyneMono text-7xl md:text-8xl  text-center text-yellow-500">Burgers</h1>
         </div>
-          <Card image={burger} title="Playa Burger" description="Carne con tocino, lechuga, jitomate y queso, acompanados con papas " price={70}/>
-          <Card image={burger} title="Coral Burger" description="Carne con tocino, lechuga, jitomate, queso y cebolla asada, acompanados con papas" price="75"/>
-          <Card image={burger} title="Manglar Burger" description="Carne con tocino, lechuga, jitomate, queso y champiñones, acompanados con papas" price="80"/>
-          <Card image={burger} title="Hawaiiana Burger" description="Carne con tocino, lechuga, jitomate,  queso, jamonon y piña, acompanados con papas" price="85"/>
-          <Card image={burger} title="Ocean Burger" description="Carne con tocino, lechuga, jitomate, cebolla asada y queso azul, acompanados con papas" price="100"/>
-          <Card image={burger} title="De La Casa" description="Carne con tocino, lechuga, jitomate, champiñones y piña, acompanados con papas" price="95"/>
+          <Card image={burger} title="Playa Burger" description="Carne con tocino, lechuga, jitomate y queso, acompañados con papas " price={70}/>
+          <Card image={burger} title="Coral Burger" description="Carne con tocino, lechuga, jitomate, queso y cebolla asada, acompañados con papas" price="75"/>
+          <Card image={burger} title="Manglar Burger" description="Carne con tocino, lechuga, jitomate, queso y champiñones, acompañados con papas" price="80"/>
+          <Card image={burger} title="Hawaiiana Burger" description="Carne con tocino, lechuga, jitomate,  queso, jamonon y piña, acompañados con papas" price="85"/>
+          <Card image={burger} title="Ocean Burger" description="Carne con tocino, lechuga, jitomate, cebolla asada y queso azul, acompañados con papas" price="100"/>
+          <Card image={burger} title="De La Casa" description="Carne con tocino, lechuga, jitomate, champiñones y piña, acompañados con papas" price="95"/>
         <div>
           <h1 className="font-SyneMono text-7xl md:text-8xl  text-center text-yellow-500">Papas</h1>
         </div>
