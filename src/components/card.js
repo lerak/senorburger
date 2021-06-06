3d5d183aebb955a483e66d71e8145249582e1128
Karel Boer <karel1982@gmail.com>
2021-06-05 23:40:35 -0500

    changes menu

--- src/components/card.js
+++ src/components/card.js
@@ -1,26 +1,35 @@
 import React from 'react'
-import delacasa from '../images/BURGERBAR_campagne-website_BURGER_01.png'
+import PropTypes from 'prop-types';
 
 
-function Card(){
+
+function Card(props){
   return(
     <div>
       <div className="z-1 overflow-visible flex items-stretch grid grid-cols-3 gap-10  text-white rounded p-5 m-5">
           <div className="1 self-center">
             <div className="p-4 ">
-              <img className="rounded object-cover"src={delacasa} alt=""/>
+              <img className="rounded object-cover"src={props.image} alt=""/>
             </div>
           </div>
           <div className="c2 self-center ">
             <div className="">
-              <h4 className=" text-2xl md:text-3xl  font-SyneMono text-white">Playa <span className="text-gray-400">Burger</span></h4>
+              <h4 className=" text-2xl md:text-3xl  font-SyneMono text-white">{props.title}</h4>
             </div>
-            <p className="text-yellow-500 text-1xl md:text-2xl">Carne con tocino, jitomate y queso</p>
+            <p className="text-yellow-500 text-1xl md:text-2xl">{props.description}</p>
           </div>
-          <div className="self-center text-yellow-500 text-4xl md:text-6xl">$65</div>
+          <div className="self-center text-yellow-500 text-4xl md:text-6xl">${props.price}</div>
       </div>
     </div>
   )
 }
+
+
+Card.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  price: PropTypes.any, 
+  image: PropTypes.any
+};
 
 export default Card
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Card from '../components/card'
+import burger from '../images/BURGERBAR_campagne-website_BURGER_01.png'
+import fries from '../images/fries.png'
+import soda from '../images/soda.png'
 // import burger from '../images/hamburger-cheeseburger-veggie-burger-fast-food-burguer-d02a8de84e0ede0b8987bbe0f866487d.png'
 import "./menu.css"
 function MenuPage() {
@@ -13,24 +16,24 @@ function MenuPage() {
         <div>
           <h1 className="font-SyneMono text-7xl md:text-8xl  text-center text-yellow-500">Burgers</h1>
         </div>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card image={burger} title="Playa Burger" description="Carne con tocino, lechuga, jitomate y queso, acompanados con papas " price={70}/>
+          <Card image={burger} title="Coral Burger" description="Carne con tocino, lechuga, jitomate, queso y cebolla asada, acompanados con papas" price="75"/>
+          <Card image={burger} title="Manglar Burger" description="Carne con tocino, lechuga, jitomate, queso y champiñones, acompanados con papas" price="80"/>
+          <Card image={burger} title="Hawaiiana Burger" description="Carne con tocino, lechuga, jitomate,  queso, jamonon y piña, acompanados con papas" price="85"/>
+          <Card image={burger} title="Ocean Burger" description="Carne con tocino, lechuga, jitomate, cebolla asada y queso azul, acompanados con papas" price="100"/>
+          <Card image={burger} title="De La Casa" description="Carne con tocino, lechuga, jitomate, champiñones y piña, acompanados con papas" price="95"/>
         <div>
-          <h1 className="font-SyneMono text-7xl md:text-8xl  text-center text-yellow-500">Extras</h1>
+          <h1 className="font-SyneMono text-7xl md:text-8xl  text-center text-yellow-500">Papas</h1>
         </div>
-          <Card />
-          <Card />
-          <Card />
+          <Card image={fries} title="Papas frito" description="Portion de papas" price="40"/>
+          <Card image={fries} title="Papas con Queso" description="portion de papas con queso amarillo" price="50"/>
         <div>
           <h1 className="font-SyneMono text-7xl md:text-8xl  text-center text-yellow-500">Refrescos</h1>
         </div>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card image={soda} title="Coca Cola" price="18"/>
+          <Card image={soda} title="Agua de Tamarindo" price="18"/>
+          <Card image={soda} title="Agua de Jamaica" price="18"/>
+          <Card image={soda} title="Agua de Horchata" price="18"/>
       </div>
     </Layout>
   )
