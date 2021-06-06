7686ef37e5ef3c190e2161b73379c3a9cd7f34f7
Karel Boer <karel1982@gmail.com>
2021-06-06 12:30:37 -0500

    change email form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,20 +11,7 @@ function ContactPage() {
         title="Contact"
       />
       <section>
-        <form>
-          <h2 className="mb-8 leading-loose">
-            Here is an example of a form built using the official Tailwind CSS Forms plugin.{` `}
-            <a
-              className="font-bold text-gray-700 no-underline"
-              href="https://github.com/tailwindlabs/tailwindcss-forms"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read the docs
-            </a>
-            .
-          </h2>
-
+        <form method="POST" data-netlify="true">
           <label
             className="block mb-2 text-xs font-bold uppercase"
             htmlFor="first-name"
@@ -35,7 +22,7 @@ function ContactPage() {
           <input
             className="w-full mb-6 rounded-md border-gray-800"
             id="first-name"
-            placeholder="Bill"
+            placeholder="Nombre"
             type="text"
           />
 
@@ -49,7 +36,7 @@ function ContactPage() {
           <input
             className="w-full mb-6 rounded-md border-gray-800"
             id="last-name"
-            placeholder="Murray"
+            placeholder="Apelido"
             type="text"
           />
 
@@ -68,7 +55,7 @@ function ContactPage() {
           />
 
           <button className="px-4 py-2 text-sm font-bold text-white bg-gray-700 border-b-4 border-gray-800 rounded hover:border-gray-700 hover:bg-gray-600">
-            Submit
+            Enviar
           </button>
         </form>
       </section>
