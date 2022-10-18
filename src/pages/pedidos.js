489d70b042c81f2982b72e14438d833c12d01630
Karel Boer <karel1982@gmail.com>
2022-10-18 17:08:22 -0500

    add order link to whatsapp

--- src/pages/pedidos.js
+++ src/pages/pedidos.js
@@ -3,7 +3,7 @@
 
 function PedidosPage(){
    
-  window.location.replace('https://api.whatsapp.com/send?phone=529841864986&text=Hola%2C%20me%20gustaria%20hacer%20un%20pedido.');
+  location.replace('https://api.whatsapp.com/send?phone=529841864986&text=Hola%2C%20me%20gustaria%20hacer%20un%20pedido.');
 }
 
 export default PedidosPage;
