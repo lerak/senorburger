5d18b78601083504409ae83de7a34205c9a0ec26
Karel Boer <karel1982@gmail.com>
2022-12-03 04:36:18 -0500

    change price drinks

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -97,13 +97,6 @@ function MenuPage() {
           <Card title="Extra Aguacate" price="20" />
           <Card title="Orden de Guacamole" price="50" />
           </div>
-          <div>
-            <h1 className="font-SyneMono text-5xl md:text-6xl text-center text-yellow-500">Sandwiches</h1>
-            <Card image={burger} title="Sandwich Guacamaya" description="Pan Ciabatta con queso de cabra, manzana, miel maple, jamon Serrano y Arugula." price="130"/>
-            <Card image={burger} title="Sandwich Pelicano" description="Pan Ciabatta con queso Mozarella, jamon Serrano y Arugula." price="125" />
-            <Card image={burger} title="Sandwich Abierto Flamingo" description="Rebenada[2] de Pan Sourdough con queso crema, Salmon y Alcapras" price="125"/>
-
-            </div>
         <div>
           <h1 className="font-SyneMono text-5xl md:text-6xl text-center text-yellow-500">Bebidas</h1>
         </div>
