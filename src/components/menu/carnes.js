aeb9dab569b9926d7cf119f85b11becb47bab7d0
Karel Boer <karel1982@gmail.com>
2023-01-16 14:54:23 -0500

    change menu description new york

--- src/components/menu/carnes.js
+++ src/components/menu/carnes.js
@@ -7,7 +7,7 @@ function Carnes(){
     <>
     <div>
       <h1 id="carnes" className="font-SyneMono text-5xl md:text-6xl text-center text-yellow-500">Carnes</h1>
-      <Carne image={burger} title="Filete New York" description="Filete New york acompañado de papas fritas y Guacamole" price="259" />
+      <Carne image={burger} title="Filete New York" description="¡Disfruta de un filete New York de primera calidad con la combinación perfecta de papas fritas y guacamole fresco!"  descriptionEN="Enjoy a top-notch New York Steak paired with the perfect combination of crispy fries and fresh guacamole!" price="259" />
     </div>
     </>
   )
