d2a2b0e1adb9df757393b5f80f6f99001999f094
Karel Boer <karel1982@gmail.com>
2023-03-13 14:47:39 -0500

    change menu description new york

--- src/components/menu/hotdogs.js
+++ src/components/menu/hotdogs.js
@@ -7,7 +7,7 @@ function Hotdogs(){
     <div>
     <h1 id="hotdogs" className="font-SyneMono text-5xl md:text-6xl text-center text-yellow-500">Hotdogs</h1>
   </div>
-    <Hotdog image={hotdog} title="Hotdog del chef" description="Salchicha Krakauer con cebolla frita, cebolla fresca y Sweet Relish y mostaza acompañado de papas fritas" descriptionEN="Krakauer Sausage with Fried Onion, Fresh Onion and Sweet Relish and Mustard Accompanied by French Fries." price="124"/>
+    <Hotdog image={hotdog} title="Hotdog del chef" description="Salchicha Krakauer con cebolla frita, cebolla fresca y Sweet Relish y mostaza acompañado de papas fritas" descriptionEN="Krakauer Sausage with Fried Onion, Fresh Onion and Sweet Relish and Mustard Accompanied by French Fries." price="134"/>
   </>
   )
 }
