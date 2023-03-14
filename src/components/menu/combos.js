83da1c2c96dfab304ec375b79d43044171022403
Karel Boer <karel1982@gmail.com>
2023-03-14 09:02:22 -0500

    change menu description new york

--- src/components/menu/combos.js
+++ src/components/menu/combos.js
@@ -8,8 +8,8 @@ function Combos(){
     <h1 id="combos" className="font-SyneMono text-5xl md:text-6xl text-center text-yellow-500">Combos</h1>
   </div>
     <Combo image={burger} title="Combo Individual" description="1 Playa Burger con papas y 1 bebida" descriptionEN="1 Playa burger with fries and 1 beverage" price="169"/>
-    <Combo image={burger} title="Combo Pareja" description="2 Playa burgers con papas y 2 bebidas" descriptionEN="2 Playa burgers with fries and 2 beverages" price="319"/>
-    <Combo image={burger} title="Combo Familiar" description="4 Playa burgers con papas y 4 bebidas" descriptionEN="4 Playa burgers with fries and 4 beverages" price="649"/>
+    <Combo image={burger} title="Combo Pareja" description="2 Playa burgers con papas y 2 bebidas" descriptionEN="2 Playa burgers with fries and 2 beverages" price="339"/>
+    <Combo image={burger} title="Combo Familiar" description="4 Playa burgers con papas y 4 bebidas" descriptionEN="4 Playa burgers with fries and 4 beverages" price="699"/>
     </>
   )
 }
